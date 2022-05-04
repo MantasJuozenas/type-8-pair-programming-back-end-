16f9a500b275db53f6c3705dd0dfea46ed8cd062
Mantas <m.juozenas@gmail.com>
2022-05-04 12:14:50 +0300

    getting medications route

--- src/routes/medicationsRoutes.js
+++ src/routes/medicationsRoutes.js
@@ -4,4 +4,19 @@ const { dbConfig } = require('../config');
 
 const medicationsRoutes = express.Router();
 
+medicationsRoutes.get('/medications', async (req, res) => {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    const sql = 'SELECT * FROM medications';
+    const [result] = await conn.query(sql);
+    res.json(result);
+  } catch (error) {
+    console.log('error in getting medications ===', error);
+    res.status(500).json({ err: 'something is wrong' });
+  } finally {
+    await conn?.end();
+  }
+});
+
 module.exports = medicationsRoutes;
