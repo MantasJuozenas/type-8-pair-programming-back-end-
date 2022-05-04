9f95bae2135eca46a0c3688dac2a5b90df0b05d6
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-05-04 11:59:26 +0300

    delete is working

--- src/routes/petsRoutes.js
+++ src/routes/petsRoutes.js
@@ -18,5 +18,54 @@ petsRoutes.get('/pets', async (req, res) => {
     await conn?.end();
   }
 });
+petsRoutes.post('/pets', async (req, res) => {
+  let conn;
+  try {
+    const { name, dob, client_email } = req.body;
+    if (name === '' || dob === '' || client_email === '') {
+      res.json({ err: 'blogai ivesti duomenys' });
+      return;
+    }
+    conn = await mysql.createConnection(dbConfig);
+    const sql = 'INSERT INTO pets (name, dob, client_email) VALUES (?, ?, ?)';
+    const [result] = await conn.execute(sql, [name, dob, client_email]);
+    if (result.affectedRows === 1) {
+      res.json({ success: 'pets post successfully created' });
+    }
+    throw new Error('something went wrong posting pets');
+  } catch (error) {
+    console.log('error in getting pets===', error);
+    res.status(500).json({ err: 'something is wrong' });
+  } finally {
+    await conn?.end();
+  }
+});
+petsRoutes.delete('/pets/:petsId', async (req, res) => {
+  let conn;
+  try {
+    const { petsId } = req.params;
+    console.log('petsId ===', petsId);
+    conn = await mysql.createConnection(dbConfig);
+    const sql = 'UPDATE pets SET archived = 1 WHERE id = ?';
+    const [result] = await conn.execute(sql, [petsId]);
+    if (result.affectedRows !== 1) {
+      res
+        .status(400)
+        .json({ success: false, error: `user with id ${petsId} was not found` });
+      return;
+    }
+
+    if (result.affectedRows === 1) {
+      res.json('delete ok');
+      return;
+    }
+    throw new Error('something wrong in deleting pets');
+  } catch (error) {
+    console.log('error in getting pets===', error);
+    res.status(500).json({ err: 'something is wrong' });
+  } finally {
+    await conn?.end();
+  }
+});
 
 module.exports = petsRoutes;
